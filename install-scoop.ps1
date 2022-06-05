8736b3bb1200b97f62e7dd7662e2f1049013068f
haya3218 <haya3218@users.noreply.github.com>
2022-06-05 23:43:04 +0000

    deploy: 9d79b21565d20997772438f34b011df320f4ad9b

--- install-scoop.ps1
+++ install-scoop.ps1
@@ -1,2 +1,4 @@
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-irm get.scoop.sh | iex
+$env:SCOOP_GLOBAL='c:\GlobalScoopApps';[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
+$env:SCOOP='c:\Scoop';[Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'MACHINE');iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+$Reg='Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment';$OldPath=(Get-ItemProperty -Path $Reg -Name PATH).Path;$NewPath=$OldPath+';'+'c:\Scoop\shims';Set-ItemProperty -Path $Reg -Name PATH -Value $NewPath
+ $CurrentValue=[Environment]::GetEnvironmentVariable('PSModulePath','Machine') ;[Environment]::SetEnvironmentVariable('PSModulePath', $CurrentValue + ';c:\Scoop\modules', 'Machine')}"
